bc933193b412c81fed42f936fce3e812c97de823
misspatriana <justinquidit5@gmail.com>
2025-03-11 16:31:18 +0800

    midterm
    palindrome

--- MIDTERM/src/midterm_jusitn/palindrome.java
+++ MIDTERM/src/midterm_jusitn/palindrome.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package midterm_jusitn;
 
-/**
- *
- * @author Administrator
- */
+import java.util.Scanner; // Import Scanner class
+
 public class palindrome {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+          System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+       
+
+        // Remove spaces and convert to lowercase
+        String cleaned = original.replaceAll("\\s+", "").toLowerCase();
+
+        // Reverse the string
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        // Check if palindrome
+        if (cleaned.equals(reversed)) {
+            System.out.println("The string is a palindrome!");
+            System.out.println(original);
+            System.out.println(cleaned);
+              System.out.println(reversed);
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+            System.out.println(original);
+            System.out.println(cleaned);
+              System.out.println(reversed);
+            
+        }
+    }
 }
