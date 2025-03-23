3770a5c9079a70ac2378ffe85819ff68bc816da0
justinquidit <justinquidit5@gmail.com>
2025-03-24 05:43:10 +0800

    Add files via upload

---
+++ MIDTERM/src/midterm/basic_calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean u = true;
+        while (u){
+            
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        double result = 0;
+
+        if (op == '+') result = num1 + num2;
+        else if (op == '-') result = num1 - num2;
+        else if (op == '*') result = num1 * num2;
+        else if (op == '/') result = num2 != 0 ? num1 / num2 : Double.NaN;
+        else System.out.println("Invalid operator!");
+
+        System.out.println("Result: " + result);
+        }
+    }
+}
